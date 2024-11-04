85ad5e81fcdde6b36a00d5c762bd91f3b1f81902
Juan Cruz Viotti <jv@jviotti.com>
2024-11-04 11:47:15 -0400

    Benchmark OMC schema compilation time (#126)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ benchmark/compiler_2019_09.cc
@@ -0,0 +1,25 @@
+#include <benchmark/benchmark.h>
+
+#include <cassert> // assert
+
+#include <sourcemeta/jsontoolkit/json.h>
+#include <sourcemeta/jsontoolkit/jsonschema.h>
+
+#include <sourcemeta/blaze/compiler.h>
+
+static void Compiler_2019_09_OMC_JSON_V2(benchmark::State &state) {
+  const auto schema{sourcemeta::jsontoolkit::from_file(
+      std::filesystem::path{CURRENT_DIRECTORY} / "schemas" /
+      "2019_09_omc_json_v2.json")};
+
+  for (auto _ : state) {
+    auto result{sourcemeta::blaze::compile(
+        schema, sourcemeta::jsontoolkit::default_schema_walker,
+        sourcemeta::jsontoolkit::official_resolver,
+        sourcemeta::blaze::default_schema_compiler)};
+    assert(!result.empty());
+    benchmark::DoNotOptimize(result);
+  }
+}
+
+BENCHMARK(Compiler_2019_09_OMC_JSON_V2);
