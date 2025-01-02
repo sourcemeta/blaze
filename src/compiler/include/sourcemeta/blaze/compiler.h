5d8d76103edac70da34d17ed00fa14796e7f8ce6
Juan Cruz Viotti <jv@jviotti.com>
2025-01-02 14:47:52 -0400

    Upgrade JSON Toolkit to `affd6a279d6797a5e9f4ddc4aec200faff14c077`
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/compiler/include/sourcemeta/blaze/compiler.h
+++ src/compiler/include/sourcemeta/blaze/compiler.h
@@ -88,9 +88,7 @@ struct Context {
   /// The root schema resource
   const sourcemeta::jsontoolkit::JSON &root;
   /// The reference frame of the entire schema
-  const sourcemeta::jsontoolkit::FrameLocations &frame;
-  /// The references of the entire schema
-  const sourcemeta::jsontoolkit::FrameReferences &references;
+  const sourcemeta::jsontoolkit::Frame &frame;
   /// The set of all schema resources in the schema without duplicates
   const std::vector<std::string> resources;
   /// The schema walker in use
