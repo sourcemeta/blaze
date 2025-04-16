02a246f9f5aafe737af67ae0e6c7a5e57afa59de
Juan Cruz Viotti <jv@jviotti.com>
2025-04-16 13:45:56 -0400

    Collect and expose annotations in `SimpleOutput` (#402)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/compiler/compiler_output_simple_test.cc
+++ test/compiler/compiler_output_simple_test.cc
@@ -15,6 +15,41 @@
   EXPECT_EQ(sourcemeta::core::to_string(traces.at((index)).evaluate_path),     \
             expected_evaluate_path);
 
+#define EXPECT_ANNOTATION_COUNT(output, expected_count)                        \
+  EXPECT_EQ(output.annotations().size(), (expected_count));
+
+#define EXPECT_ANNOTATION_ENTRY(output, expected_instance_location,            \
+                                expected_evaluate_path, expected_entry_count)  \
+  {                                                                            \
+    const auto instance_location{                                              \
+        sourcemeta::core::to_pointer(expected_instance_location)};             \
+    const auto evaluate_path{                                                  \
+        sourcemeta::core::to_pointer(expected_evaluate_path)};                 \
+    EXPECT_TRUE(output.annotations().contains(                                 \
+        {sourcemeta::core::to_weak_pointer(instance_location),                 \
+         sourcemeta::core::to_weak_pointer(evaluate_path)}));                  \
+    EXPECT_EQ(output.annotations()                                             \
+                  .at({sourcemeta::core::to_weak_pointer(instance_location),   \
+                       sourcemeta::core::to_weak_pointer(evaluate_path)})      \
+                  .size(),                                                     \
+              (expected_entry_count));                                         \
+  }
+
+#define EXPECT_ANNOTATION_VALUE(output, expected_instance_location,            \
+                                expected_evaluate_path, expected_entry_index,  \
+                                expected_value)                                \
+  {                                                                            \
+    const auto instance_location{                                              \
+        sourcemeta::core::to_pointer(expected_instance_location)};             \
+    const auto evaluate_path{                                                  \
+        sourcemeta::core::to_pointer(expected_evaluate_path)};                 \
+    EXPECT_EQ(output.annotations()                                             \
+                  .at({sourcemeta::core::to_weak_pointer(instance_location),   \
+                       sourcemeta::core::to_weak_pointer(evaluate_path)})      \
+                  .at(expected_entry_index),                                   \
+              (expected_value));                                               \
+  }
+
 TEST(Compiler_output_simple, success_string_1) {
   const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
     "$schema": "https://json-schema.org/draft/2020-12/schema",
@@ -37,6 +72,8 @@ TEST(Compiler_output_simple, success_string_1) {
   std::vector<sourcemeta::blaze::SimpleOutput::Entry> traces{output.cbegin(),
                                                              output.cend()};
   EXPECT_TRUE(traces.empty());
+
+  EXPECT_ANNOTATION_COUNT(output, 0);
 }
 
 TEST(Compiler_output_simple, fail_meaningless_if_1) {
@@ -82,6 +119,8 @@ TEST(Compiler_output_simple, fail_meaningless_if_1) {
   EXPECT_OUTPUT(
       traces, 1, "/foo", "/properties/foo/unevaluatedProperties",
       "The object value was not expected to define unevaluated properties");
+
+  EXPECT_ANNOTATION_COUNT(output, 0);
 }
 
 TEST(Compiler_output_simple, success_dynamic_anchor_1) {
@@ -112,6 +151,8 @@ TEST(Compiler_output_simple, success_dynamic_anchor_1) {
   std::vector<sourcemeta::blaze::SimpleOutput::Entry> traces{output.cbegin(),
                                                              output.cend()};
   EXPECT_TRUE(traces.empty());
+
+  EXPECT_ANNOTATION_COUNT(output, 0);
 }
 
 TEST(Compiler_output_simple, success_oneof_1) {
@@ -139,6 +180,8 @@ TEST(Compiler_output_simple, success_oneof_1) {
   std::vector<sourcemeta::blaze::SimpleOutput::Entry> traces{output.cbegin(),
                                                              output.cend()};
   EXPECT_TRUE(traces.empty());
+
+  EXPECT_ANNOTATION_COUNT(output, 0);
 }
 
 TEST(Compiler_output_simple, fail_string) {
@@ -167,6 +210,8 @@ TEST(Compiler_output_simple, fail_string) {
   EXPECT_OUTPUT(
       traces, 0, "", "/type",
       "The value was expected to be of type string but it was of type integer");
+
+  EXPECT_ANNOTATION_COUNT(output, 0);
 }
 
 TEST(Compiler_output_simple, fail_string_over_ref) {
@@ -200,6 +245,7 @@ TEST(Compiler_output_simple, fail_string_over_ref) {
   EXPECT_OUTPUT(
       traces, 0, "", "/$ref/type",
       "The value was expected to be of type string but it was of type integer");
+  EXPECT_ANNOTATION_COUNT(output, 0);
 }
 
 TEST(Compiler_output_simple, fail_string_with_matching_base) {
@@ -235,6 +281,7 @@ TEST(Compiler_output_simple, fail_string_with_matching_base) {
   EXPECT_OUTPUT(
       traces, 0, "", "/type",
       "The value was expected to be of type string but it was of type integer");
+  EXPECT_ANNOTATION_COUNT(output, 0);
 }
 
 TEST(Compiler_output_simple, fail_string_with_non_matching_base) {
@@ -269,6 +316,7 @@ TEST(Compiler_output_simple, fail_string_with_non_matching_base) {
   EXPECT_OUTPUT(
       traces, 0, "", "/$ref/type",
       "The value was expected to be of type string but it was of type integer");
+  EXPECT_ANNOTATION_COUNT(output, 0);
 }
 
 TEST(Compiler_output_simple, fail_oneof_1) {
@@ -300,6 +348,7 @@ TEST(Compiler_output_simple, fail_oneof_1) {
   EXPECT_OUTPUT(traces, 0, "", "/oneOf",
                 "The string value was expected to validate against one and "
                 "only one of the 2 given subschemas");
+  EXPECT_ANNOTATION_COUNT(output, 0);
 }
 
 TEST(Compiler_output_simple, fail_not_1) {
@@ -330,6 +379,7 @@ TEST(Compiler_output_simple, fail_not_1) {
   EXPECT_OUTPUT(traces, 0, "", "/not",
                 "The string value was expected to not validate against the "
                 "given subschema, but it did");
+  EXPECT_ANNOTATION_COUNT(output, 0);
 }
 
 TEST(Compiler_output_simple, fail_not_not_1) {
@@ -362,6 +412,7 @@ TEST(Compiler_output_simple, fail_not_not_1) {
   EXPECT_OUTPUT(traces, 0, "", "/not",
                 "The integer value was expected to not validate against the "
                 "given subschema, but it did");
+  EXPECT_ANNOTATION_COUNT(output, 0);
 }
 
 TEST(Compiler_output_simple, fail_anyof_1) {
@@ -400,6 +451,7 @@ TEST(Compiler_output_simple, fail_anyof_1) {
   EXPECT_OUTPUT(
       traces, 0, "", "/allOf/1/type",
       "The value was expected to be of type integer but it was of type object");
+  EXPECT_ANNOTATION_COUNT(output, 0);
 }
 
 TEST(Compiler_output_simple, fail_anyof_2) {
@@ -433,4 +485,150 @@ TEST(Compiler_output_simple, fail_anyof_2) {
   EXPECT_OUTPUT(traces, 0, "", "/anyOf",
                 "The object value was expected to validate against at least "
                 "one of the 2 given subschemas");
+  EXPECT_ANNOTATION_COUNT(output, 0);
+}
+
+TEST(Compiler_output_simple, annotations_success_1) {
+  const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "title": "My Schema",
+    "properties": {
+      "foo": {
+        "title": "Foo",
+        "deprecated": true,
+        "$ref": "#/$defs/test"
+      },
+      "bar": true
+    },
+    "$defs": {
+      "test": {
+        "title": "Test"
+      }
+    }
+  })JSON")};
+
+  const auto schema_template{sourcemeta::blaze::compile(
+      schema, sourcemeta::core::schema_official_walker,
+      sourcemeta::core::schema_official_resolver,
+      sourcemeta::blaze::default_schema_compiler,
+      sourcemeta::blaze::Mode::Exhaustive)};
+
+  const auto instance{sourcemeta::core::parse_json(R"JSON({
+    "foo": "bar",
+    "bar": "baz"
+  })JSON")};
+
+  sourcemeta::blaze::SimpleOutput output{instance};
+  sourcemeta::blaze::Evaluator evaluator;
+  const auto result{
+      evaluator.validate(schema_template, instance, std::ref(output))};
+  EXPECT_TRUE(result);
+
+  EXPECT_ANNOTATION_COUNT(output, 5);
+
+  EXPECT_ANNOTATION_ENTRY(output, "", "/properties", 2);
+  EXPECT_ANNOTATION_VALUE(output, "", "/properties", 0,
+                          sourcemeta::core::JSON{"bar"});
+  EXPECT_ANNOTATION_VALUE(output, "", "/properties", 1,
+                          sourcemeta::core::JSON{"foo"});
+
+  EXPECT_ANNOTATION_ENTRY(output, "", "/title", 1);
+  EXPECT_ANNOTATION_VALUE(output, "", "/title", 0,
+                          sourcemeta::core::JSON{"My Schema"});
+
+  EXPECT_ANNOTATION_ENTRY(output, "/foo", "/properties/foo/$ref/title", 1);
+  EXPECT_ANNOTATION_VALUE(output, "/foo", "/properties/foo/$ref/title", 0,
+                          sourcemeta::core::JSON{"Test"});
+
+  EXPECT_ANNOTATION_ENTRY(output, "/foo", "/properties/foo/title", 1);
+  EXPECT_ANNOTATION_VALUE(output, "/foo", "/properties/foo/title", 0,
+                          sourcemeta::core::JSON{"Foo"});
+
+  EXPECT_ANNOTATION_ENTRY(output, "/foo", "/properties/foo/deprecated", 1);
+  EXPECT_ANNOTATION_VALUE(output, "/foo", "/properties/foo/deprecated", 0,
+                          sourcemeta::core::JSON{true});
+}
+
+TEST(Compiler_output_simple, annotations_success_2) {
+  const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "anyOf": [
+      { "title": "First branch" },
+      { "type": "string", "title": "Second branch" },
+      { "title": "Third branch" }
+    ]
+  })JSON")};
+
+  const auto schema_template{sourcemeta::blaze::compile(
+      schema, sourcemeta::core::schema_official_walker,
+      sourcemeta::core::schema_official_resolver,
+      sourcemeta::blaze::default_schema_compiler,
+      sourcemeta::blaze::Mode::Exhaustive)};
+
+  const sourcemeta::core::JSON instance{5};
+
+  sourcemeta::blaze::SimpleOutput output{instance};
+  sourcemeta::blaze::Evaluator evaluator;
+  const auto result{
+      evaluator.validate(schema_template, instance, std::ref(output))};
+  EXPECT_TRUE(result);
+
+  EXPECT_ANNOTATION_COUNT(output, 2);
+
+  EXPECT_ANNOTATION_ENTRY(output, "", "/anyOf/0/title", 1);
+  EXPECT_ANNOTATION_VALUE(output, "", "/anyOf/0/title", 0,
+                          sourcemeta::core::JSON{"First branch"});
+
+  EXPECT_ANNOTATION_ENTRY(output, "", "/anyOf/2/title", 1);
+  EXPECT_ANNOTATION_VALUE(output, "", "/anyOf/2/title", 0,
+                          sourcemeta::core::JSON{"Third branch"});
+}
+
+TEST(Compiler_output_simple, annotations_success_3) {
+  const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "not": {
+      "title": "First branch",
+      "type": "string"
+    }
+  })JSON")};
+
+  const auto schema_template{sourcemeta::blaze::compile(
+      schema, sourcemeta::core::schema_official_walker,
+      sourcemeta::core::schema_official_resolver,
+      sourcemeta::blaze::default_schema_compiler,
+      sourcemeta::blaze::Mode::Exhaustive)};
+
+  const sourcemeta::core::JSON instance{5};
+
+  sourcemeta::blaze::SimpleOutput output{instance};
+  sourcemeta::blaze::Evaluator evaluator;
+  const auto result{
+      evaluator.validate(schema_template, instance, std::ref(output))};
+  EXPECT_TRUE(result);
+
+  EXPECT_ANNOTATION_COUNT(output, 0);
+}
+
+TEST(Compiler_output_simple, annotations_failure_1) {
+  const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "title": "Foo",
+    "type": "string"
+  })JSON")};
+
+  const auto schema_template{sourcemeta::blaze::compile(
+      schema, sourcemeta::core::schema_official_walker,
+      sourcemeta::core::schema_official_resolver,
+      sourcemeta::blaze::default_schema_compiler,
+      sourcemeta::blaze::Mode::Exhaustive)};
+
+  const sourcemeta::core::JSON instance{5};
+
+  sourcemeta::blaze::SimpleOutput output{instance};
+  sourcemeta::blaze::Evaluator evaluator;
+  const auto result{
+      evaluator.validate(schema_template, instance, std::ref(output))};
+  EXPECT_FALSE(result);
+  EXPECT_ANNOTATION_COUNT(output, 0);
 }
