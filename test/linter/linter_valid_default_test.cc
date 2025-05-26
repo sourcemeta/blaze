a859726dfe0600aefc117212db500d3f342e09a0
Juan Cruz Viotti <jv@jviotti.com>
2025-05-26 14:13:27 -0300

    Fix `default`/`examples` linting with references to relative IDs (#436)
    Fixes: https://github.com/sourcemeta/jsonschema/issues/338
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/linter/linter_valid_default_test.cc
+++ test/linter/linter_valid_default_test.cc
@@ -309,3 +309,45 @@ TEST(Linter, valid_default_8) {
 
   EXPECT_EQ(schema, expected);
 }
+
+TEST(Linter, valid_default_9) {
+  sourcemeta::core::SchemaTransformer bundle;
+  bundle.add<sourcemeta::blaze::ValidDefault>(
+      sourcemeta::blaze::default_schema_compiler);
+
+  auto schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "properties": {
+      "foo": { "default": 1 },
+      "bar": { "$ref": "ref.schema.json" }
+    }
+  })JSON")};
+
+  auto resolver = [](const std::string_view identifier)
+      -> std::optional<sourcemeta::core::JSON> {
+    if (identifier == "ref.schema.json") {
+      return sourcemeta::core::parse_json(R"JSON({
+        "$schema": "https://json-schema.org/draft/2020-12/schema",
+        "$id": "ref.schema.json"
+      })JSON");
+    }
+
+    return sourcemeta::core::schema_official_resolver(identifier);
+  };
+
+  const auto result =
+      bundle.apply(schema, sourcemeta::core::schema_official_walker, resolver,
+                   transformer_callback_error);
+
+  EXPECT_TRUE(result);
+
+  const auto expected{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "properties": {
+      "foo": { "default": 1 },
+      "bar": { "$ref": "ref.schema.json" }
+    }
+  })JSON")};
+
+  EXPECT_EQ(schema, expected);
+}
--- test/linter/linter_valid_examples_test.cc
+++ test/linter/linter_valid_examples_test.cc
@@ -325,7 +325,7 @@ TEST(Linter, valid_examples_8) {
 
 TEST(Linter, valid_examples_9) {
   sourcemeta::core::SchemaTransformer bundle;
-  bundle.add<sourcemeta::blaze::ValidDefault>(
+  bundle.add<sourcemeta::blaze::ValidExamples>(
       sourcemeta::blaze::default_schema_compiler);
 
   auto schema{sourcemeta::core::parse_json(R"JSON({
@@ -344,6 +344,48 @@ TEST(Linter, valid_examples_9) {
     "$schema": "http://json-schema.org/draft-07/schema#",
     "additionalProperties": false,
     "examples": [ {} ]
+  })JSON")};
+
+  EXPECT_EQ(schema, expected);
+}
+
+TEST(Linter, valid_examples_10) {
+  sourcemeta::core::SchemaTransformer bundle;
+  bundle.add<sourcemeta::blaze::ValidExamples>(
+      sourcemeta::blaze::default_schema_compiler);
+
+  auto schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "properties": {
+      "foo": { "examples": [ 1 ] },
+      "bar": { "$ref": "ref.schema.json" }
+    }
+  })JSON")};
+
+  auto resolver = [](const std::string_view identifier)
+      -> std::optional<sourcemeta::core::JSON> {
+    if (identifier == "ref.schema.json") {
+      return sourcemeta::core::parse_json(R"JSON({
+        "$schema": "https://json-schema.org/draft/2020-12/schema",
+        "$id": "ref.schema.json"
+      })JSON");
+    }
+
+    return sourcemeta::core::schema_official_resolver(identifier);
+  };
+
+  const auto result =
+      bundle.apply(schema, sourcemeta::core::schema_official_walker, resolver,
+                   transformer_callback_error);
+
+  EXPECT_TRUE(result);
+
+  const auto expected{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "properties": {
+      "foo": { "examples": [ 1 ] },
+      "bar": { "$ref": "ref.schema.json" }
+    }
   })JSON")};
 
   EXPECT_EQ(schema, expected);
