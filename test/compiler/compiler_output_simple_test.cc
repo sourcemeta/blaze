b2525db645648128075fc8260fa96681740b96ec
Juan Cruz Viotti <jv@jviotti.com>
2025-06-06 15:24:21 -0400

    Drop annotations from `contains` that were emitted to non-matching items (#447)
    Fixes: https://github.com/sourcemeta/blaze/issues/446
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/compiler/compiler_output_simple_test.cc
+++ test/compiler/compiler_output_simple_test.cc
@@ -803,6 +803,36 @@ TEST(Compiler_output_simple, annotations_success_9) {
                           0, sourcemeta::core::JSON{true});
 }
 
+TEST(Compiler_output_simple, annotations_success_10) {
+  const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "contains": { "type": "number", "title": "Test" }
+  })JSON")};
+
+  const auto schema_template{sourcemeta::blaze::compile(
+      schema, sourcemeta::core::schema_official_walker,
+      sourcemeta::core::schema_official_resolver,
+      sourcemeta::blaze::default_schema_compiler,
+      sourcemeta::blaze::Mode::Exhaustive)};
+
+  const sourcemeta::core::JSON instance{
+      sourcemeta::core::parse_json("[ \"foo\", 42, true ]")};
+
+  sourcemeta::blaze::SimpleOutput output{instance};
+  sourcemeta::blaze::Evaluator evaluator;
+  const auto result{
+      evaluator.validate(schema_template, instance, std::ref(output))};
+  EXPECT_TRUE(result);
+
+  EXPECT_ANNOTATION_COUNT(output, 2);
+
+  EXPECT_ANNOTATION_ENTRY(output, "", "/contains", "#/contains", 1);
+  EXPECT_ANNOTATION_ENTRY(output, "/1", "/contains/title", "#/contains/title",
+                          1);
+  EXPECT_ANNOTATION_VALUE(output, "/1", "/contains/title", "#/contains/title",
+                          0, sourcemeta::core::JSON{"Test"});
+}
+
 TEST(Compiler_output_simple, annotations_failure_1) {
   const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
     "$schema": "https://json-schema.org/draft/2020-12/schema",
