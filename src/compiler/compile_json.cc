33ae3b86f1c5b9e1140e9047b251746df7ce70ca
Juan Cruz Viotti <jv@jviotti.com>
2025-06-24 09:27:30 -0400

    Aggressive
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/compiler/compile_json.cc
+++ src/compiler/compile_json.cc
@@ -2,19 +2,6 @@
 
 #include <variant> // std::visit
 
-namespace sourcemeta::core {
-template <typename T>
-  requires std::is_same_v<T, sourcemeta::blaze::ValueNone>
-auto to_json(const T &) -> JSON {
-  return JSON{nullptr};
-}
-template <typename T>
-  requires std::is_same_v<T, sourcemeta::blaze::ValueRegex>
-auto to_json(const T &value) -> JSON {
-  return JSON{value.second};
-}
-} // namespace sourcemeta::core
-
 namespace sourcemeta::blaze {
 
 auto to_json(const Instruction &instruction) -> sourcemeta::core::JSON {
