026cdcfc33f821644211b9b5f9663bb5e942d316
Juan Cruz Viotti <jv@jviotti.com>
2025-06-30 15:30:28 -0400

    Support the `Flag` and `Basic` standard output formats
    See: https://github.com/sourcemeta/jsonschema/issues/213
    See: https://github.com/sourcemeta/jsonschema/issues/58
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ test/compiler/compiler_output_standard_flag_test.cc
@@ -0,0 +1,94 @@
+#include <gtest/gtest.h>
+
+#include <sourcemeta/blaze/compiler.h>
+#include <sourcemeta/blaze/evaluator.h>
+
+TEST(Compiler_output_standard_flag, success_1) {
+  const auto schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "properties": {
+      "foo": { "type": "string" }
+    }
+  })JSON")};
+
+  const auto schema_template{sourcemeta::blaze::compile(
+      schema, sourcemeta::core::schema_official_walker,
+      sourcemeta::core::schema_official_resolver,
+      sourcemeta::blaze::default_schema_compiler,
+      sourcemeta::blaze::Mode::FastValidation)};
+
+  const auto instance{sourcemeta::core::parse_json(R"JSON({
+    "foo": "bar"
+  })JSON")};
+
+  sourcemeta::blaze::Evaluator evaluator;
+  const auto result{
+      sourcemeta::blaze::standard(evaluator, schema_template, instance,
+                                  sourcemeta::blaze::StandardOutput::Flag)};
+
+  const auto expected{sourcemeta::core::parse_json(R"JSON({
+    "valid": true
+  })JSON")};
+
+  EXPECT_EQ(result, expected);
+}
+
+TEST(Compiler_output_standard_flag, success_1_exhaustive) {
+  const auto schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "properties": {
+      "foo": { "type": "string" }
+    }
+  })JSON")};
+
+  const auto schema_template{sourcemeta::blaze::compile(
+      schema, sourcemeta::core::schema_official_walker,
+      sourcemeta::core::schema_official_resolver,
+      sourcemeta::blaze::default_schema_compiler,
+      sourcemeta::blaze::Mode::Exhaustive)};
+
+  const auto instance{sourcemeta::core::parse_json(R"JSON({
+    "foo": "bar"
+  })JSON")};
+
+  sourcemeta::blaze::Evaluator evaluator;
+  const auto result{
+      sourcemeta::blaze::standard(evaluator, schema_template, instance,
+                                  sourcemeta::blaze::StandardOutput::Flag)};
+
+  const auto expected{sourcemeta::core::parse_json(R"JSON({
+    "valid": true
+  })JSON")};
+
+  EXPECT_EQ(result, expected);
+}
+
+TEST(Compiler_output_standard_flag, failure_1) {
+  const auto schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "properties": {
+      "foo": { "type": "string" }
+    }
+  })JSON")};
+
+  const auto schema_template{sourcemeta::blaze::compile(
+      schema, sourcemeta::core::schema_official_walker,
+      sourcemeta::core::schema_official_resolver,
+      sourcemeta::blaze::default_schema_compiler,
+      sourcemeta::blaze::Mode::FastValidation)};
+
+  const auto instance{sourcemeta::core::parse_json(R"JSON({
+    "foo": 1
+  })JSON")};
+
+  sourcemeta::blaze::Evaluator evaluator;
+  const auto result{
+      sourcemeta::blaze::standard(evaluator, schema_template, instance,
+                                  sourcemeta::blaze::StandardOutput::Flag)};
+
+  const auto expected{sourcemeta::core::parse_json(R"JSON({
+    "valid": false
+  })JSON")};
+
+  EXPECT_EQ(result, expected);
+}
