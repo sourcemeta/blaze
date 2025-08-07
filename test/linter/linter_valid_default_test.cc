f3276b0b526657e32612144e6256f022eb0f81f0
Juan Cruz Viotti <jv@jviotti.com>
2025-08-07 16:38:12 -0400

    Don't lint `examples` and `default` in Draft 7 and older given `$ref`
    Fixes: https://github.com/sourcemeta/blaze/issues/493
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/linter/linter_valid_default_test.cc
+++ test/linter/linter_valid_default_test.cc
@@ -573,3 +573,71 @@ TEST(Linter, valid_default_12) {
 
   EXPECT_EQ(schema, expected);
 }
+
+TEST(Linter, valid_default_13) {
+  sourcemeta::core::SchemaTransformer bundle;
+  bundle.add<sourcemeta::blaze::ValidDefault>(
+      sourcemeta::blaze::default_schema_compiler);
+
+  auto schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "properties": {
+      "foo": { "$ref": "#/definitions/helper", "default": 1 }
+    },
+    "definitions": {
+      "helper": { "type": "string" }
+    }
+  })JSON")};
+
+  const auto result = bundle.apply(
+      schema, sourcemeta::core::schema_official_walker,
+      sourcemeta::core::schema_official_resolver, transformer_callback_error);
+
+  EXPECT_TRUE(result);
+
+  const auto expected{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "properties": {
+      "foo": { "$ref": "#/definitions/helper", "default": 1 }
+    },
+    "definitions": {
+      "helper": { "type": "string" }
+    }
+  })JSON")};
+
+  EXPECT_EQ(schema, expected);
+}
+
+TEST(Linter, valid_default_14) {
+  sourcemeta::core::SchemaTransformer bundle;
+  bundle.add<sourcemeta::blaze::ValidDefault>(
+      sourcemeta::blaze::default_schema_compiler);
+
+  auto schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "properties": {
+      "foo": { "$ref": "#/definitions/helper", "default": 1 }
+    },
+    "definitions": {
+      "helper": { "type": "string" }
+    }
+  })JSON")};
+
+  const auto result = bundle.apply(
+      schema, sourcemeta::core::schema_official_walker,
+      sourcemeta::core::schema_official_resolver, transformer_callback_error);
+
+  EXPECT_TRUE(result);
+
+  const auto expected{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "properties": {
+      "foo": { "$ref": "#/definitions/helper" }
+    },
+    "definitions": {
+      "helper": { "type": "string" }
+    }
+  })JSON")};
+
+  EXPECT_EQ(schema, expected);
+}
--- test/linter/linter_valid_examples_test.cc
+++ test/linter/linter_valid_examples_test.cc
@@ -615,3 +615,71 @@ TEST(Linter, valid_examples_13) {
 
   EXPECT_EQ(schema, expected);
 }
+
+TEST(Linter, valid_examples_14) {
+  sourcemeta::core::SchemaTransformer bundle;
+  bundle.add<sourcemeta::blaze::ValidExamples>(
+      sourcemeta::blaze::default_schema_compiler);
+
+  auto schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "properties": {
+      "foo": { "$ref": "#/definitions/helper", "examples": [ 1 ] }
+    },
+    "definitions": {
+      "helper": { "type": "string" }
+    }
+  })JSON")};
+
+  const auto result = bundle.apply(
+      schema, sourcemeta::core::schema_official_walker,
+      sourcemeta::core::schema_official_resolver, transformer_callback_error);
+
+  EXPECT_TRUE(result);
+
+  const auto expected{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "properties": {
+      "foo": { "$ref": "#/definitions/helper", "examples": [ 1 ] }
+    },
+    "definitions": {
+      "helper": { "type": "string" }
+    }
+  })JSON")};
+
+  EXPECT_EQ(schema, expected);
+}
+
+TEST(Linter, valid_examples_15) {
+  sourcemeta::core::SchemaTransformer bundle;
+  bundle.add<sourcemeta::blaze::ValidExamples>(
+      sourcemeta::blaze::default_schema_compiler);
+
+  auto schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "properties": {
+      "foo": { "$ref": "#/definitions/helper", "examples": [ 1 ] }
+    },
+    "definitions": {
+      "helper": { "type": "string" }
+    }
+  })JSON")};
+
+  const auto result = bundle.apply(
+      schema, sourcemeta::core::schema_official_walker,
+      sourcemeta::core::schema_official_resolver, transformer_callback_error);
+
+  EXPECT_TRUE(result);
+
+  const auto expected{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "properties": {
+      "foo": { "$ref": "#/definitions/helper" }
+    },
+    "definitions": {
+      "helper": { "type": "string" }
+    }
+  })JSON")};
+
+  EXPECT_EQ(schema, expected);
+}
