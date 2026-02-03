f6c55c4beb013673bc881a31b23336d0d6366a6f
Juan Cruz Viotti <jv@jviotti.com>
2026-02-03 12:09:16 -0400

    Compare some Node.js benchmark cases against AJV
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- bindings/nodejs/benchmark.js
+++ bindings/nodejs/benchmark.js
@@ -3,6 +3,9 @@ import { readFileSync, readdirSync, statSync, existsSync } from 'fs';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { compileSchema, evaluateSchema, BlazeJSON } from '@sourcemeta/blaze';
+import { Ajv2020 } from 'ajv/dist/2020.js';
+import { Ajv2019 } from 'ajv/dist/2019.js';
+import Ajv from 'ajv';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const E2E_DIR = join(__dirname, '../../benchmark/e2e');
@@ -31,31 +34,46 @@ if (benchmarksToRun.length === 0) {
 }
 
 const suite = new Benchmark.Suite();
-const benchmarkData = new Map();
-for (const name of benchmarksToRun) {
-  benchmarkData.set(name, {
-    schema:
-      compileSchema(
-        BlazeJSON.parse(readFileSync(join(E2E_DIR, name, 'schema.json'), 'utf8')), {
-          mode: 'fast'
-        }),
-    instances: readFileSync(join(E2E_DIR, name, 'instances.jsonl'), 'utf8')
-      .trim()
-      .split('\n')
-      .map(line => BlazeJSON.parse(line))
-  });
-}
 
 for (const name of benchmarksToRun) {
-  const { schema, instances } = benchmarkData.get(name);
+  const schemaText = readFileSync(join(E2E_DIR, name, 'schema.json'), 'utf8');
+  const instancesText = readFileSync(join(E2E_DIR, name, 'instances.jsonl'), 'utf8');
+
+  const compiledSchema = compileSchema(BlazeJSON.parse(schemaText), { mode: 'fast' });
+  const blazeInstances = instancesText.trim().split('\n').map(line => BlazeJSON.parse(line));
   suite.add(`E2E_Evaluator_${formatName(name)}`, () => {
-    for (const instance of instances) {
-      const result = evaluateSchema(schema, instance);
+    for (const instance of blazeInstances) {
+      const result = evaluateSchema(compiledSchema, instance);
       if (!result.valid) {
         throw new Error(`Validation failed for ${name}`);
       }
     }
   });
+
+  if (name != "krakend" && name != "ui5-manifest") {
+    console.log(`Trying ${name}`);
+    const AJV_DRAFTS = {
+      "https://json-schema.org/draft/2020-12/schema": Ajv2020,
+      "https://json-schema.org/draft/2020-12/schema#": Ajv2020,
+      "https://json-schema.org/draft/2019-09/schema": Ajv2019,
+      "https://json-schema.org/draft/2019-09/schema#": Ajv2019,
+      "http://json-schema.org/draft-07/schema": Ajv,
+      "http://json-schema.org/draft-07/schema#": Ajv
+    };
+
+    const schema = JSON.parse(schemaText);
+    const AjvClass = AJV_DRAFTS[schema['$schema']];
+    const ajv = new AjvClass({ strict: false });
+    const validate = ajv.compile(schema);
+    const ajvInstances = instancesText.trim().split('\n').map(line => JSON.parse(line));
+    suite.add(`E2E_Evaluator_${formatName(name)}_ajv`, () => {
+      for (const instance of ajvInstances) {
+        if (!validate(instance)) {
+          throw new Error(`Validation failed for ${name}`);
+        }
+      }
+    });
+  }
 }
 
 suite
