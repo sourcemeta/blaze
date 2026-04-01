80f15da71c1147346c5375c971ca5621aaa18fbd
Juan Cruz Viotti <jv@jviotti.com>
2026-04-01 13:27:38 -0400

    [WIP] Fix dynamic references traces mismatched after roundtrip
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/evaluator/evaluator_2020_12_test.cc
+++ test/evaluator/evaluator_2020_12_test.cc
@@ -32,6 +32,95 @@ TEST(Evaluator_2020_12, metaschema_hyper_self_exhaustive) {
                                          207, "");
 }
 
+TEST(Evaluator_2020_12, dynamicRef_with_multiple_anchors) {
+  const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "$dynamicRef": "https://example.com/target#meta",
+    "$defs": {
+      "target": {
+        "$id": "https://example.com/target",
+        "$dynamicAnchor": "meta",
+        "type": "boolean"
+      },
+      "other": {
+        "$id": "https://example.com/other",
+        "$dynamicAnchor": "meta",
+        "type": "number"
+      }
+    }
+  })JSON")};
+
+  const sourcemeta::core::JSON instance{true};
+  EVALUATE_WITH_TRACE_FAST_SUCCESS(schema, instance, 2, "");
+
+  EVALUATE_TRACE_PRE(0, ControlDynamicAnchorJump, "/$dynamicRef",
+                     "#/$dynamicRef", "");
+  EVALUATE_TRACE_PRE(1, AssertionTypeStrict, "/$dynamicRef/type",
+                     "https://example.com/target#/type", "");
+
+  EVALUATE_TRACE_POST_SUCCESS(0, AssertionTypeStrict, "/$dynamicRef/type",
+                              "https://example.com/target#/type", "");
+  EVALUATE_TRACE_POST_SUCCESS(1, ControlDynamicAnchorJump, "/$dynamicRef",
+                              "#/$dynamicRef", "");
+
+  EVALUATE_TRACE_POST_DESCRIBE(instance, 0,
+                               "The value was expected to be of type boolean");
+  EVALUATE_TRACE_POST_DESCRIBE(
+      instance, 1,
+      "The boolean value was expected to validate against the first subschema "
+      "in scope that declared the dynamic anchor \"meta\"");
+}
+
+TEST(Evaluator_2020_12, dynamicRef_with_multiple_anchors_from_json) {
+  const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "$dynamicRef": "https://example.com/target#meta",
+    "$defs": {
+      "target": {
+        "$id": "https://example.com/target",
+        "$dynamicAnchor": "meta",
+        "type": "boolean"
+      },
+      "other": {
+        "$id": "https://example.com/other",
+        "$dynamicAnchor": "meta",
+        "type": "number"
+      }
+    }
+  })JSON")};
+
+  const auto original{sourcemeta::blaze::compile(
+      schema, sourcemeta::core::schema_walker,
+      sourcemeta::core::schema_resolver,
+      sourcemeta::blaze::default_schema_compiler,
+      sourcemeta::blaze::Mode::FastValidation)};
+
+  const auto roundtripped{
+      sourcemeta::blaze::from_json(sourcemeta::blaze::to_json(original))};
+  ASSERT_TRUE(roundtripped.has_value());
+
+  const sourcemeta::core::JSON instance{true};
+  EVALUATE_WITH_TRACE(roundtripped.value(), instance, 2);
+  EXPECT_TRUE(result);
+
+  EVALUATE_TRACE_PRE(0, ControlDynamicAnchorJump, "/$dynamicRef",
+                     "https://example.com/target#/$dynamicRef", "");
+  EVALUATE_TRACE_PRE(1, AssertionTypeStrict, "/$dynamicRef/type",
+                     "https://example.com/target#/type", "");
+
+  EVALUATE_TRACE_POST_SUCCESS(0, AssertionTypeStrict, "/$dynamicRef/type",
+                              "https://example.com/target#/type", "");
+  EVALUATE_TRACE_POST_SUCCESS(1, ControlDynamicAnchorJump, "/$dynamicRef",
+                              "https://example.com/target#/$dynamicRef", "");
+
+  EVALUATE_TRACE_POST_DESCRIBE(instance, 0,
+                               "The value was expected to be of type boolean");
+  EVALUATE_TRACE_POST_DESCRIBE(
+      instance, 1,
+      "The boolean value was expected to validate against the first subschema "
+      "in scope that declared the dynamic anchor \"meta\"");
+}
+
 TEST(Evaluator_2020_12, reference_from_unknown_keyword) {
   const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
     "$schema": "https://json-schema.org/draft/2020-12/schema",
