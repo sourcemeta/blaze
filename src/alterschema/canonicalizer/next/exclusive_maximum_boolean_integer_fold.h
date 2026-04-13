c989de98f3fa68283f188546f897f6924c77061e
Juan Cruz Viotti <jv@jviotti.com>
2026-04-13 12:44:44 -0400

    Fix decimal conversion issues in the Draft 4 canonicalizer
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/alterschema/canonicalizer/next/exclusive_maximum_boolean_integer_fold.h
+++ src/alterschema/canonicalizer/next/exclusive_maximum_boolean_integer_fold.h
@@ -32,9 +32,16 @@ public:
   auto transform(JSON &schema, const Result &) const -> void override {
     const auto &maximum{schema.at("maximum")};
     if (maximum.is_integer()) {
-      auto new_maximum = maximum;
-      new_maximum += sourcemeta::core::JSON{-1};
-      schema.assign("maximum", std::move(new_maximum));
+      if (maximum.to_integer() > std::numeric_limits<std::int64_t>::min()) {
+        auto new_maximum = maximum;
+        new_maximum += sourcemeta::core::JSON{-1};
+        schema.assign("maximum", std::move(new_maximum));
+      } else {
+        auto result{
+            sourcemeta::core::Decimal{std::to_string(maximum.to_integer())}};
+        result -= sourcemeta::core::Decimal{1};
+        schema.assign("maximum", sourcemeta::core::JSON{std::move(result)});
+      }
     } else if (maximum.is_decimal()) {
       auto current{maximum.to_decimal()};
       auto floored{current.to_integral()};
@@ -53,12 +60,31 @@ public:
       }
     } else {
       const auto value{maximum.to_real()};
-      auto floored{static_cast<std::int64_t>(std::floor(value))};
-      if (std::floor(value) == value) {
-        floored -= 1;
-      }
+      const auto floor_value{std::floor(value)};
+      const auto result{(floor_value == value) ? floor_value - 1.0
+                                               : floor_value};
+      if (std::isfinite(result) &&
+          result >=
+              static_cast<double>(std::numeric_limits<std::int64_t>::min()) &&
+          result <
+              static_cast<double>(std::numeric_limits<std::int64_t>::max()) +
+                  1.0) {
+        schema.assign("maximum", sourcemeta::core::JSON{
+                                     static_cast<std::int64_t>(result)});
+      } else {
+        auto decimal_result{sourcemeta::core::Decimal{value}};
+        if (decimal_result.is_integer()) {
+          decimal_result -= sourcemeta::core::Decimal{1};
+        } else {
+          decimal_result = decimal_result.to_integral();
+          if (decimal_result > sourcemeta::core::Decimal{value}) {
+            decimal_result -= sourcemeta::core::Decimal{1};
+          }
+        }
 
-      schema.assign("maximum", sourcemeta::core::JSON{floored});
+        schema.assign("maximum",
+                      sourcemeta::core::JSON{std::move(decimal_result)});
+      }
     }
 
     schema.erase("exclusiveMaximum");
