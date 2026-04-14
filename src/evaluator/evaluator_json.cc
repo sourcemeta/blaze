236dcc4bf76f054ecbd52b6ee07e97d88722e51c
Juan Cruz Viotti <jv@jviotti.com>
2026-04-14 16:31:38 -0400

    More
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/evaluator/evaluator_json.cc
+++ src/evaluator/evaluator_json.cc
@@ -40,35 +40,8 @@ auto value_from_json(const sourcemeta::core::JSON &wrapper)
     case 19: return sourcemeta::core::from_json<ValueTypedProperties>(value);
     case 20: return sourcemeta::core::from_json<ValueStringHashes>(value);
     case 21: return sourcemeta::core::from_json<ValueTypedHashes>(value);
-    case 22:
-      if (value.is_array() && value.array_size() == 2 &&
-          value.at(0).is_integer() && value.at(1).is_integer()) {
-        return ValueIntegerBounds{value.at(0).to_integer(),
-                                  value.at(1).to_integer()};
-      }
-      return std::nullopt;
-    case 23:
-      if (value.is_array() && value.array_size() == 2) {
-        const auto &bounds_json{value.at(0)};
-        const auto &range_json{value.at(1)};
-        if (bounds_json.is_array() && bounds_json.array_size() == 2 &&
-            bounds_json.at(0).is_integer() &&
-            bounds_json.at(1).is_integer() &&
-            range_json.is_array() && range_json.array_size() == 3 &&
-            range_json.at(0).is_integer() && range_json.at(2).is_boolean()) {
-          return ValueIntegerBoundsWithSize{
-              ValueIntegerBounds{bounds_json.at(0).to_integer(),
-                                bounds_json.at(1).to_integer()},
-              ValueRange{
-                  static_cast<std::size_t>(range_json.at(0).to_integer()),
-                  range_json.at(1).is_null()
-                      ? std::nullopt
-                      : std::optional<std::size_t>{static_cast<std::size_t>(
-                            range_json.at(1).to_integer())},
-                  range_json.at(2).to_boolean()}};
-        }
-      }
-      return std::nullopt;
+    case 22: return sourcemeta::core::from_json<ValueIntegerBounds>(value);
+    case 23: return sourcemeta::core::from_json<ValueIntegerBoundsWithSize>(value);
     // clang-format on
     default:
       std::unreachable();
