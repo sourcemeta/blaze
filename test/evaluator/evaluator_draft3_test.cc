9ded71e16c48cc23a71b9d5798bcad4c61a43818
Juan Cruz Viotti <jv@jviotti.com>
2026-05-05 16:30:02 -0400

    Add `enum` Draft 3 trace tests (#783)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/evaluator/evaluator_draft3_test.cc
+++ test/evaluator/evaluator_draft3_test.cc
@@ -81,3 +81,37 @@ TEST(Evaluator_draft3,
       "The value was expected to be of type integer, or string but it was of "
       "type number");
 }
+
+TEST(Evaluator_draft3, enum_real_value_equals_integer_member_fast) {
+  const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "http://json-schema.org/draft-03/schema#",
+    "enum": [ 3 ]
+  })JSON")};
+
+  const sourcemeta::core::JSON instance{sourcemeta::core::parse_json("3.0")};
+
+  EVALUATE_WITH_TRACE_FAST_SUCCESS(schema, instance, 1, "");
+
+  EVALUATE_TRACE_PRE(0, AssertionEqual, "/enum", "#/enum", "");
+  EVALUATE_TRACE_POST_SUCCESS(0, AssertionEqual, "/enum", "#/enum", "");
+  EVALUATE_TRACE_POST_DESCRIBE(
+      instance, 0,
+      "The number value 3.0 was expected to equal the integer constant 3");
+}
+
+TEST(Evaluator_draft3, enum_real_value_equals_integer_member_exhaustive) {
+  const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "http://json-schema.org/draft-03/schema#",
+    "enum": [ 3 ]
+  })JSON")};
+
+  const sourcemeta::core::JSON instance{sourcemeta::core::parse_json("3.0")};
+
+  EVALUATE_WITH_TRACE_EXHAUSTIVE_SUCCESS(schema, instance, 1, "");
+
+  EVALUATE_TRACE_PRE(0, AssertionEqual, "/enum", "#/enum", "");
+  EVALUATE_TRACE_POST_SUCCESS(0, AssertionEqual, "/enum", "#/enum", "");
+  EVALUATE_TRACE_POST_DESCRIBE(
+      instance, 0,
+      "The number value 3.0 was expected to equal the integer constant 3");
+}
