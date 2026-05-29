6f28486e3f9b79aa2993f1ffa42e704e76431c63
Juan Cruz Viotti <jv@jviotti.com>
2026-05-29 15:46:12 -0400

    OpenAPI tests
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/evaluator/evaluator_openapi_3_1_test.cc
+++ test/evaluator/evaluator_openapi_3_1_test.cc
@@ -7,3 +7,155 @@
 #include <sourcemeta/core/json.h>
 
 #include "evaluator_utils.h"
+
+TEST(Evaluator_openapi_3_1, format_uri_valid_with_tweak_fast) {
+  const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://spec.openapis.org/oas/3.1/dialect/base",
+    "format": "uri"
+  })JSON")};
+
+  const sourcemeta::core::JSON instance{"https://example.com"};
+
+  sourcemeta::blaze::Tweaks tweaks;
+  tweaks.format_assertion = true;
+
+  EVALUATE_WITH_TRACE_FAST_SUCCESS_TWEAKED(schema, instance, 1, "", tweaks);
+
+  EVALUATE_TRACE_PRE(0, AssertionStringType, "/format", "#/format", "");
+  EVALUATE_TRACE_POST_SUCCESS(0, AssertionStringType, "/format", "#/format",
+                              "");
+  EVALUATE_TRACE_POST_DESCRIBE(
+      instance, 0,
+      "The string value \"https://example.com\" was expected to represent a "
+      "valid URI");
+}
+
+TEST(Evaluator_openapi_3_1, format_uri_invalid_with_tweak_fast) {
+  const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://spec.openapis.org/oas/3.1/dialect/base",
+    "format": "uri"
+  })JSON")};
+
+  const sourcemeta::core::JSON instance{"://bad"};
+
+  sourcemeta::blaze::Tweaks tweaks;
+  tweaks.format_assertion = true;
+
+  EVALUATE_WITH_TRACE_FAST_FAILURE_TWEAKED(schema, instance, 1, "", tweaks);
+
+  EVALUATE_TRACE_PRE(0, AssertionStringType, "/format", "#/format", "");
+  EVALUATE_TRACE_POST_FAILURE(0, AssertionStringType, "/format", "#/format",
+                              "");
+  EVALUATE_TRACE_POST_DESCRIBE(
+      instance, 0,
+      "The string value \"://bad\" was expected to represent a valid URI");
+}
+
+TEST(Evaluator_openapi_3_1, format_uri_valid_with_tweak_exhaustive) {
+  const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://spec.openapis.org/oas/3.1/dialect/base",
+    "format": "uri"
+  })JSON")};
+
+  const sourcemeta::core::JSON instance{"https://example.com"};
+
+  sourcemeta::blaze::Tweaks tweaks;
+  tweaks.format_assertion = true;
+
+  EVALUATE_WITH_TRACE_EXHAUSTIVE_SUCCESS_TWEAKED(schema, instance, 2, "",
+                                                 tweaks);
+
+  EVALUATE_TRACE_PRE(0, AssertionStringType, "/format", "#/format", "");
+  EVALUATE_TRACE_PRE_ANNOTATION(1, "/format", "#/format", "");
+
+  EVALUATE_TRACE_POST_SUCCESS(0, AssertionStringType, "/format", "#/format",
+                              "");
+  EVALUATE_TRACE_POST_ANNOTATION(1, "/format", "#/format", "", "uri");
+
+  EVALUATE_TRACE_POST_DESCRIBE(
+      instance, 0,
+      "The string value \"https://example.com\" was expected to represent a "
+      "valid URI");
+  EVALUATE_TRACE_POST_DESCRIBE(
+      instance, 1,
+      "The logical type of the instance was expected to be \"uri\"");
+}
+
+TEST(Evaluator_openapi_3_1, format_uri_invalid_with_tweak_exhaustive) {
+  const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://spec.openapis.org/oas/3.1/dialect/base",
+    "format": "uri"
+  })JSON")};
+
+  const sourcemeta::core::JSON instance{"://bad"};
+
+  sourcemeta::blaze::Tweaks tweaks;
+  tweaks.format_assertion = true;
+
+  EVALUATE_WITH_TRACE_EXHAUSTIVE_FAILURE_TWEAKED(schema, instance, 1, "",
+                                                 tweaks);
+
+  EVALUATE_TRACE_PRE(0, AssertionStringType, "/format", "#/format", "");
+  EVALUATE_TRACE_POST_FAILURE(0, AssertionStringType, "/format", "#/format",
+                              "");
+  EVALUATE_TRACE_POST_DESCRIBE(
+      instance, 0,
+      "The string value \"://bad\" was expected to represent a valid URI");
+}
+
+TEST(Evaluator_openapi_3_1, format_uri_no_tweak_fast) {
+  const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://spec.openapis.org/oas/3.1/dialect/base",
+    "format": "uri"
+  })JSON")};
+
+  const sourcemeta::core::JSON instance{"://bad"};
+
+  EVALUATE_WITH_TRACE_FAST_SUCCESS(schema, instance, 0, "");
+}
+
+TEST(Evaluator_openapi_3_1, format_uri_no_tweak_exhaustive) {
+  const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://spec.openapis.org/oas/3.1/dialect/base",
+    "format": "uri"
+  })JSON")};
+
+  const sourcemeta::core::JSON instance{"://bad"};
+
+  EVALUATE_WITH_TRACE_EXHAUSTIVE_SUCCESS(schema, instance, 1, "");
+
+  EVALUATE_TRACE_PRE_ANNOTATION(0, "/format", "#/format", "");
+  EVALUATE_TRACE_POST_ANNOTATION(0, "/format", "#/format", "", "uri");
+  EVALUATE_TRACE_POST_DESCRIBE(
+      instance, 0,
+      "The logical type of the instance was expected to be \"uri\"");
+}
+
+TEST(Evaluator_openapi_3_1, format_uri_non_string_with_tweak_fast) {
+  const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://spec.openapis.org/oas/3.1/dialect/base",
+    "format": "uri"
+  })JSON")};
+
+  const sourcemeta::core::JSON instance{3.14};
+
+  sourcemeta::blaze::Tweaks tweaks;
+  tweaks.format_assertion = true;
+
+  EVALUATE_WITH_TRACE_FAST_SUCCESS_TWEAKED(schema, instance, 0, "", tweaks);
+}
+
+TEST(Evaluator_openapi_3_1, format_uri_non_string_with_tweak_exhaustive) {
+  const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://spec.openapis.org/oas/3.1/dialect/base",
+    "format": "uri"
+  })JSON")};
+
+  const sourcemeta::core::JSON instance{3.14};
+
+  sourcemeta::blaze::Tweaks tweaks;
+  tweaks.format_assertion = true;
+
+  EVALUATE_WITH_TRACE_EXHAUSTIVE_SUCCESS_TWEAKED(schema, instance, 0, "",
+                                                 tweaks);
+}
