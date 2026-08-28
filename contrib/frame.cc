a8b72142ed3e6a5b23e3cc669e055cb102de6d1f
Juan Cruz Viotti <jv@jviotti.com>
2026-08-28 12:14:55 -0300

    Add a contrib tool for quick framing
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ contrib/frame.cc
@@ -0,0 +1,33 @@
+#include <sourcemeta/blaze/foundation.h>
+
+#include <sourcemeta/core/json.h>
+
+#include <cstdlib>  // EXIT_SUCCESS, EXIT_FAILURE
+#include <iostream> // std::cerr, std::cin, std::cout
+
+auto main(int argc, char *argv[]) -> int {
+  if (argc > 2) {
+    std::cerr << "Usage: " << argv[0] << " [schema.json]\n"
+              << "Reads the schema from standard input if no path is given\n";
+    return EXIT_FAILURE;
+  }
+
+  try {
+    const auto schema{argc == 2 ? sourcemeta::core::read_json(argv[1])
+                                : sourcemeta::core::parse_json(std::cin)};
+
+    sourcemeta::blaze::SchemaFrame frame{
+        sourcemeta::blaze::SchemaFrame::Mode::References};
+    frame.analyse(schema, sourcemeta::blaze::schema_walker,
+                  sourcemeta::blaze::schema_resolver);
+
+    sourcemeta::core::prettify(
+        frame.to_json(sourcemeta::blaze::schema_resolver), std::cout);
+    std::cout << "\n";
+  } catch (const std::exception &error) {
+    std::cerr << error.what() << "\n";
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
