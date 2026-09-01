0be895aa8316807f6d1655572d8e319345616425
Juan Cruz Viotti <jv@jviotti.com>
2026-09-01 12:00:31 -0300

    Better pull in only necessary Core libraries (#1019)
    See: https://github.com/sourcemeta/blaze/issues/1017
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- cmake/FindCore.cmake
+++ cmake/FindCore.cmake
@@ -12,6 +12,31 @@ if(NOT Core_FOUND)
 
   set(SOURCEMETA_CORE_CONTRIB_GOOGLETEST ${BLAZE_TESTS} CACHE BOOL "GoogleTest")
   set(SOURCEMETA_CORE_CONTRIB_GOOGLEBENCHMARK ${BLAZE_BENCHMARK} CACHE BOOL "GoogleBenchmark")
+
+  # Core builds every one of its libraries by default. Blaze only needs a
+  # subset of them, and consumers otherwise end up with a pile of libraries
+  # they have no use for, plus the OpenSSL and cURL dependencies that some of
+  # them bring in
+  set(SOURCEMETA_CORE_HTML OFF CACHE BOOL "unused by Blaze")
+  set(SOURCEMETA_CORE_HTTP OFF CACHE BOOL "unused by Blaze")
+  set(SOURCEMETA_CORE_JOSE OFF CACHE BOOL "unused by Blaze")
+  set(SOURCEMETA_CORE_JSONPATH OFF CACHE BOOL "unused by Blaze")
+  set(SOURCEMETA_CORE_JSONRPC OFF CACHE BOOL "unused by Blaze")
+  set(SOURCEMETA_CORE_MARKDOWN OFF CACHE BOOL "unused by Blaze")
+  set(SOURCEMETA_CORE_MCP OFF CACHE BOOL "unused by Blaze")
+  set(SOURCEMETA_CORE_OAUTH OFF CACHE BOOL "unused by Blaze")
+  set(SOURCEMETA_CORE_OIDC OFF CACHE BOOL "unused by Blaze")
+  set(SOURCEMETA_CORE_SEMVER OFF CACHE BOOL "unused by Blaze")
+  set(SOURCEMETA_CORE_LANG_PARALLEL OFF CACHE BOOL "unused by Blaze")
+
+  # Only the test suite makes use of the Core testing library, which in turn
+  # is what pulls in these three
+  set(SOURCEMETA_CORE_LANG_TEST ${BLAZE_TESTS} CACHE BOOL "Core test library")
+  set(SOURCEMETA_CORE_DIFF ${BLAZE_TESTS} CACHE BOOL "Core test library only")
+  set(SOURCEMETA_CORE_LANG_PROCESS ${BLAZE_TESTS}
+    CACHE BOOL "Core test library only")
+  set(SOURCEMETA_CORE_LANG_STACKTRACE ${BLAZE_TESTS}
+    CACHE BOOL "Core test library only")
   add_subdirectory("${PROJECT_SOURCE_DIR}/vendor/core")
   include(Sourcemeta)
   set(Core_FOUND ON)
