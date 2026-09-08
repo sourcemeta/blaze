e9ddd90e8c1b5b89e6f95660f6bbc5502c34bea6
Juan Cruz Viotti <jv@jviotti.com>
2026-09-08 15:23:53 -0300

    Upgrade Core to `756006c5222ec7ae1344ce2c222ae022e06bbbf9` (#1047)
    See: https://github.com/sourcemeta/blaze/issues/1017
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- vendor/core/cmake/FindGoogleBenchmark.cmake
+++ vendor/core/cmake/FindGoogleBenchmark.cmake
@@ -7,6 +7,12 @@ if(NOT Benchmark_FOUND)
 
   add_subdirectory("${PROJECT_SOURCE_DIR}/vendor/googlebenchmark")
 
+  # The option above only governs the warnings as errors that this library
+  # turns on for itself. The one that consumers set reaches every target in
+  # the build, including this one
+  set_target_properties(benchmark benchmark_main
+    PROPERTIES COMPILE_WARNING_AS_ERROR OFF)
+
   # Consumers run static analysis over their own benchmark sources, and the
   # headers this library exposes are not theirs to answer for
   get_target_property(BENCHMARK_INCLUDE_DIRECTORIES
